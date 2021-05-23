606af313d7d9fdb0f532ad3e4288cab33aeb2788
huangcs <huangcs@ushareit.com>
2021-05-23 16:15:39 +0800

    split dataset

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,6 +1,7 @@
 from math import log
 
 
+# 获取信息熵
 def get_entropy(dataSet):
     infoLen = len(dataSet)
     labelCount = {}
@@ -15,6 +16,41 @@ def get_entropy(dataSet):
         p = float(count) / infoLen
         result += p * log(p, 2)
     return -result
+
+
+# 按照给定特征lie和特征值划分数据集
+# 将满足条件的特征剔除
+def splitDataSet(dataSet, axis, value):
+    result = []
+    for data in dataSet:
+        if data[axis] == value:
+            subSet = data[:axis]
+            subSet.extend(data[axis+1:])
+            result.append(subSet)
+    return result
+
+
+# 信息增益最大的就是最高的划分特征
+# 信息增益是不同熵的差值
+def chooseBestFeature(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = get_entropy(dataSet)
+    bestInfoGain = 0.0; bestFeature = -1
+
+    for i in range(numFeatures):
+        featureList = [sample[i] for sample in dataSet]
+        uniqFeature = set(featureList)
+
+        newEntropy = 0.0
+        for feature in uniqFeature:
+            subDataSet = splitDataSet(dataSet, i, feature)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * get_entropy(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
 
 
 def createDataSet():

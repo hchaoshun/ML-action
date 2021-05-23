a0567336cbd558d523f0a54511dac00d55116a0e
huangcs <huangcs@ushareit.com>
2021-05-23 17:15:37 +0800

    create tree

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -61,3 +61,45 @@ def createDataSet():
               [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
     return dataSet, labels
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for val in classList:
+        if val not in classList.keys():
+            classCount[val] = 0
+        classCount[val] += 1
+    sortedClassCount = sorted(classCount.items(), key=lambda item : item[1], reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    # 第一个终止条件：所有label值都相同, 返回label值
+    classList = [sample[-1] for sample in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+
+    # 第二个终止条件，所有特征都用完，返回label出现次数最多的值
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+
+    bestFeature = chooseBestFeature(dataSet)
+    bestFeatureLab = labels[bestFeature]
+
+    myTree = {bestFeatureLab: {}}
+    # todo have to delete it?
+    del(labels[bestFeature])
+
+    # 递归构建树
+    featureVals = [sample[bestFeature] for sample in dataSet]
+    for val in set(featureVals):
+        myTree[bestFeatureLab][val] = createTree(splitDataSet(dataSet, bestFeature, val), labels)
+
+    return myTree
+
+
+if __name__ == '__main__':
+    myDat, labels = createDataSet()
+    myTree = createTree(myDat, labels)
+    print(myTree)
+
